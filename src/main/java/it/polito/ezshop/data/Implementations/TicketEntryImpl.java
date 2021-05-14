22bf2025187c8a6fccdc9de8b8842bf6379366b5
b3xul <bexul97@gmail.com>
2021-05-14 20:09:54 +0200

    new TicketEntry

--- src/main/java/it/polito/ezshop/data/Implementations/TicketEntryImpl.java
+++ src/main/java/it/polito/ezshop/data/Implementations/TicketEntryImpl.java
@@ -1,54 +1,86 @@
 package it.polito.ezshop.data.Implementations;
 
-import it.polito.ezshop.data.ProductType;
 import it.polito.ezshop.data.TicketEntry;
 
 public class TicketEntryImpl implements TicketEntry {
 
+	String barCode;
+	String productDescription;
+	double pricePerUnit;
+	double discountRate;
 	int amount;
-	ProductType product;
-	Double discountRate;
 
-	TicketEntryImpl(String note, String productDescription, String barCode, double pricePerUnit, double discountRate,
-			int amount){
-		this.product = new ProductTypeImpl(note, productDescription, barCode, pricePerUnit);
-		//this.product.setDiscountRate(discountRate);
-		this.amount = amount;
+	public TicketEntryImpl(String barCode, String productDescription, double pricePerUnit, double discountRate,
+			int amount) {
+
+		this.barCode = barCode;
+		this.productDescription = productDescription;
+		this.pricePerUnit = pricePerUnit;
 		this.discountRate = discountRate;
+		this.amount = amount;
+
 	}
 
 	@Override
 	public String getBarCode() {
 
-		return this.product.getBarCode();
+		return barCode;
 
 	}
 
 	@Override
 	public void setBarCode(String barCode) {
 
-		this.product.setBarCode(barCode);
+		this.barCode = barCode;
 
 	}
 
 	@Override
 	public String getProductDescription() {
 
-		return this.product.getProductDescription();
+		return productDescription;
 
 	}
 
 	@Override
 	public void setProductDescription(String productDescription) {
 
-		this.product.setProductDescription(productDescription);
+		this.productDescription = productDescription;
+
+	}
+
+	@Override
+	public double getPricePerUnit() {
+
+		return pricePerUnit;
+
+	}
+
+	@Override
+	public void setPricePerUnit(double pricePerUnit) {
+
+		this.pricePerUnit = pricePerUnit;
+
+	}
+
+	@Override
+	public double getDiscountRate() {
+
+		return discountRate;
+
+	}
+
+	@Override
+	public void setDiscountRate(double discountRate) {
+
+		this.discountRate = discountRate;
 
 	}
 
 	@Override
 	public int getAmount() {
 
-		return this.amount;
+		return amount;
 
 	}
 
@@ -57,36 +89,6 @@ public class TicketEntryImpl implements TicketEntry {
 
 		this.amount = amount;
 
-	}
-	
-	@Override 
-	public double getPricePerUnit() {
-
-		return this.product.getPricePerUnit();
-
-	}
-
-	@Override
-	public void setPricePerUnit(double pricePerUnit) {
-
-		this.product.setPricePerUnit(pricePerUnit);
-
-	}
-
-	@Override
-	public double getDiscountRate() {
-		
-		return this.discountRate;
-		//return this.getDiscountRate();
-		
-	}
-
-	@Override
-	public void setDiscountRate(double discountRate) {
-		
-		this.discountRate = discountRate;
-		//this.product.setDiscountRate(discountRate);
-		
 	}
 
 }
