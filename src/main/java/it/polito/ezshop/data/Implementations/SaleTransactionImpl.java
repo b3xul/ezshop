3bf907ee5d26c464555e768c098eae7b193f228c
b3xul <bexul97@gmail.com>
2021-05-14 21:20:11 +0200

    implemented endSaleTransaction

--- src/main/java/it/polito/ezshop/data/Implementations/SaleTransactionImpl.java
+++ src/main/java/it/polito/ezshop/data/Implementations/SaleTransactionImpl.java
@@ -4,30 +4,31 @@ import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 
-import it.polito.ezshop.data.ProductType;
 import it.polito.ezshop.data.SaleTransaction;
 import it.polito.ezshop.data.TicketEntry;
 
-public class SaleTransactionImpl extends BalanceOperationImpl implements SaleTransaction {
+public class SaleTransactionImpl implements SaleTransaction {
 
-	// Integer ticketNumber; -> BalanceOperation.balanceId
+	Integer ticketNumber;
+	double price;
 	LinkedList<TicketEntry> entries;
 	double discountRate;
-	// double price; -> BalanceOperation.money
 
 	public SaleTransactionImpl(Integer ticketNumber) {
 
-		super(ticketNumber, "saleTransaction");
+		this.ticketNumber = ticketNumber;
+		this.price = 0;
 		this.entries = new LinkedList<TicketEntry>();
 		this.discountRate = 0;
 
 	}
 
-	public void upsertEntry(ProductType productType, int amount) { // update or insert entry if it doesn't exist
+	public void upsertEntry(String barCode, String productDescription, double pricePerUnit, double discountRate,
+			int amount) { // update or insert entry if it doesn't exist
 
 		Boolean updated = false;
 		for (TicketEntry entry : entries) {
-			if (entry.getBarCode() == productType.getBarCode()) {
+			if (entry.getBarCode() == barCode) {
 				entry.setAmount(entry.getAmount() + amount);
 				updated = true;
 				System.out.println(entry);
@@ -35,18 +36,19 @@ public class SaleTransactionImpl extends BalanceOperationImpl implements SaleTra
 			}
 		}
 		if (updated == false) {
-			entries.add(new TicketEntryImpl(productType, amount));
+			entries.add(new TicketEntryImpl(barCode, productDescription, pricePerUnit, discountRate, amount));
 		}
+		// this.setPrice(this.price + amount * pricePerUnit * (1 - discountRate));
 
 	}
 
-	public Boolean removeAmountFromEntry(ProductType productType, int amountToRemove) {
+	public Boolean removeAmountFromEntry(String barCode, double pricePerUnit, double discountRate, int amountToRemove) {
 
 		Boolean updated = false;
 		Iterator<TicketEntry> iter = entries.iterator();
 		while (iter.hasNext()) {
 			TicketEntry entry = iter.next();
-			if (entry.getBarCode() == productType.getBarCode()) { // product present in the saleTransaction
+			if (entry.getBarCode() == barCode) { // product present in the saleTransaction
 				int previousAmount = entry.getAmount();
 				if (amountToRemove < previousAmount) {
 					entry.setAmount(previousAmount - amountToRemove);
@@ -55,7 +57,7 @@ public class SaleTransactionImpl extends BalanceOperationImpl implements SaleTra
 					iter.remove();
 					updated = true;
 				}
-				// else if (amountToRemove > previousAmount) updated==false;
+				// else if (amountToRemove > previousAmount) updated=false;
 				System.out.println(entry);
 				break;
 			}
@@ -65,17 +67,43 @@ public class SaleTransactionImpl extends BalanceOperationImpl implements SaleTra
 
 	}
 
+	public void setDiscountRateToProduct(String barCode, double discountRate) {
+
+		for (TicketEntry entry : entries) {
+			if (entry.getBarCode() == barCode) {
+				entry.setDiscountRate(discountRate);
+				System.out.println(entry);
+				break;
+			}
+		}
+
+	}
+
 	@Override
 	public Integer getTicketNumber() {
 
-		return this.getBalanceId();
+		return ticketNumber;
 
 	}
 
 	@Override
 	public void setTicketNumber(Integer ticketNumber) {
 
-		this.setBalanceId(ticketNumber);
+		this.ticketNumber = ticketNumber;
+
+	}
+
+	@Override
+	public double getPrice() {
+
+		return price;
+
+	}
+
+	@Override
+	public void setPrice(double price) {
+
+		this.price = price;
 
 	}
 
@@ -108,25 +136,10 @@ public class SaleTransactionImpl extends BalanceOperationImpl implements SaleTra
 	}
 
 	@Override
-	public double getPrice() {
-
-		return this.getMoney();
-
-	}
-
-	@Override
-	public void setPrice(double price) {
-
-		this.setMoney(price);
-
-	}
-
-	@Override
 	public String toString() {
 
-		return "SaleTransactionImpl [balanceId=" + this.getBalanceId() + ", date=" + this.getDate() + ", money="
-				+ this.getMoney() + ", type=" + this.getType() + " entries=" + entries + ", discountRate="
-				+ discountRate + "]";
+		return "SaleTransactionImpl [ticketNumber=" + this.getTicketNumber() + ", money=" + this.price + " entries="
+				+ entries + ", discountRate=" + discountRate + "]";
 
 	}
 
---
+++ src/test/java/it/polito/ezshop/acceptanceTests/EndSaleTransactionTest.java
@@ -0,0 +1,27 @@
+package it.polito.ezshop.acceptanceTests;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import it.polito.ezshop.data.EZShopInterface;
+import it.polito.ezshop.exceptions.InvalidProductCodeException;
+import it.polito.ezshop.exceptions.InvalidQuantityException;
+import it.polito.ezshop.exceptions.InvalidTransactionIdException;
+import it.polito.ezshop.exceptions.UnauthorizedException;
+
+public class EndSaleTransactionTest {
+
+	@Test
+	public void testCase1() throws InvalidTransactionIdException, InvalidProductCodeException, InvalidQuantityException,
+			UnauthorizedException {
+
+		EZShopInterface ezShop = new it.polito.ezshop.data.EZShop();
+		Integer tid = ezShop.startSaleTransaction();
+		System.out.println(tid);
+		assertTrue(ezShop.addProductToSale(tid, "98765432123456", 2));
+		assertTrue(ezShop.endSaleTransaction(tid));
+
+	}
+
+}
