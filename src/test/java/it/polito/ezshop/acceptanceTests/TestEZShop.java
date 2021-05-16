28da354ec868a585ee74da8f1102ca9f514f558b
Andrea Palomba <s291692@studenti.polito.it>
2021-05-16 15:10:54 +0000

    Update TestEZShop.java

--- src/test/java/it/polito/ezshop/acceptanceTests/TestEZShop.java
+++ src/test/java/it/polito/ezshop/acceptanceTests/TestEZShop.java
@@ -1,5 +1,12 @@
 package it.polito.ezshop.acceptanceTests;
 
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite.SuiteClasses;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@SuiteClasses({CreateProductTypeTest.class,UpdateProductTest.class,DeleteProductTypeTest.class,GetAllProductTypesTest.class,
+	GetProductTypeByBarCodeTest.class,GetProductTypeByDescriptionTest.class, UpdateQuantityTest.class,UpdatePositionTest.class})
 
 public class TestEZShop {
     
