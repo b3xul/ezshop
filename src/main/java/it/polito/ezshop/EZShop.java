d285bc8a0c460129f9f2e695fd475d9f846d73c6
Leonardo Perugini s249807 <s249807@studenti.polito.it>
2021-05-16 22:42:02 +0200

    updated ezshop class

--- src/main/java/it/polito/ezshop/EZShop.java
+++ src/main/java/it/polito/ezshop/EZShop.java
@@ -14,27 +14,10 @@ import it.polito.ezshop.view.EZShopGUI;
 
 public class EZShop {
 
-//    public static void main(String[] args){
-//        EZShopInterface ezShop = new it.polito.ezshop.data.EZShop();
-//        EZShopGUI gui = new EZShopGUI(ezShop);
-//    }
-	
-	public static void main(String[] args) throws InvalidProductCodeException, InvalidQuantityException, InvalidPricePerUnitException, UnauthorizedException, InvalidOrderIdException, InvalidLocationException{
-		EZShopInterface ezShop = new it.polito.ezshop.data.EZShop();
-		ezShop.payOrderFor("1234567898765", 10, 5);
-		ezShop.payOrderFor("1234567898765", 10, 5);
-		ezShop.payOrderFor("1234567898765", 10, 5);
-		ezShop.payOrderFor("1234567898765", 10, 5);
-		ezShop.payOrderFor("1234567898765", 10, 5);
-//		ezShop.getAllOrders();
-//		ezShop.recordOrderArrival(1);
-//		ezShop.issueOrder("98765432123456", 5, 10);
-//		ezShop.payOrder(2);
-//		ezShop.recordBalanceUpdate(20);
-//		ezShop.recordBalanceUpdate(10);
-//		ezShop.recordBalanceUpdate(30);
-//		ezShop.getCreditsAndDebits(LocalDate.parse("2021-01-01"), LocalDate.parse("2021-12-30"));
-//		ezShop.getCreditsAndDebits(null, LocalDate.parse("2021-12-30"));
-	}
+    public static void main(String[] args){
+        EZShopInterface ezShop = new it.polito.ezshop.data.EZShop();
+        EZShopGUI gui = new EZShopGUI(ezShop);
+    }
+
 
 }
