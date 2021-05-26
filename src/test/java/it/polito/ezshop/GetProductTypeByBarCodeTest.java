a521be849aa9321c5e7779bf58fdb952fa1a9bd4
Andrea Palomba <s291692@studenti.polito.it>
2021-05-26 15:09:40 +0000

    Update GetProductTypeByBarCodeTest.java

--- src/test/java/it/polito/ezshop/GetProductTypeByBarCodeTest.java
+++ src/test/java/it/polito/ezshop/GetProductTypeByBarCodeTest.java
@@ -7,17 +7,11 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 import it.polito.ezshop.data.EZShopInterface;
-import it.polito.ezshop.exceptions.InvalidPasswordException;
-import it.polito.ezshop.exceptions.InvalidPricePerUnitException;
-import it.polito.ezshop.exceptions.InvalidProductCodeException;
-import it.polito.ezshop.exceptions.InvalidProductDescriptionException;
-import it.polito.ezshop.exceptions.InvalidProductIdException;
-import it.polito.ezshop.exceptions.InvalidUsernameException;
-import it.polito.ezshop.exceptions.UnauthorizedException;
+import it.polito.ezshop.exceptions.*;
 
 public class GetProductTypeByBarCodeTest {
 
-	EZShopInterface ezShop = new it.polito.ezshop.data.EZShop();
+	static EZShopInterface ezShop = new it.polito.ezshop.data.EZShop();
 
 	
 	@Test
